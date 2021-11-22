a394dc3dc02cebbdd3f4d77c69e57ca43277a054
hossein shooshtari <hossein0918@gmail.com>
2021-11-22 10:10:34 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ChabokKit",
-            targets: ["ChabokKit", "RemoteBinaryPackage", "LocalBinaryPackage"])
+            targets: ["ChabokKit", "RemoteBinaryPackage"])
     ],
     dependencies: [],
     targets: [
