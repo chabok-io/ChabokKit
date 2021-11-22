cdabaaae9d7d78579b813ea8dcbb837ca0167bb8
hossein shooshtari <hossein0918@gmail.com>
2021-11-22 10:45:02 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,12 +22,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        
         .target(name: "ChabokKit",
-                path: "https://github.com/chabok-io/chabok-ios/tree/main/ChabokKit.xcframework")
-        .binaryTarget(
-            name: "RemoteBinaryPackage",
-            url: "https://github.com/chabok-io/chabok-ios/tree/main/ChabokKit.xcframework"
+                path: "https://github.com/chabok-io/chabok-ios/tree/main/ChabokKit.xcframework"
         )
     ]
 )
